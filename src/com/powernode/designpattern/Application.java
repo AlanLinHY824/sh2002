27d00a4d1878a8e03b147aee551a59b2a8f0c6a1
AlanLin <alanlin824@aliyun.com>
2020-09-18 13:57:02 +0800

    设计模式学习recommit

--- src/com/powernode/designpattern/Application.java
+++ src/com/powernode/designpattern/Application.java
@@ -6,9 +6,10 @@ import java.lang.reflect.Field;
  * @Author AlanLin
  * @Description
  * @Date 2020/9/15
+ * 测试类
  */
 public class Application {
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchFieldException {
         Class<String> stringClass = String.class;
         Field hash = stringClass.getDeclaredField("hash");
         Class<?> type = hash.getType();
